18c44d781b5db8feff064334c830f7a26aa35f2f
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-29 23:03:20 +0100

    travel list starter

---
+++ src/Travel.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+const Travel = () => {
+  return <div>Travel Component</div>;
+};
+
+export default Travel;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Date from "./Date";
+import Travel from "./Travel";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <Date />
+    <Travel />
   </React.StrictMode>
 );

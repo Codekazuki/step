5cb48be7cb8f348750c3eff380b332a31af98ed9
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-30 23:00:20 +0100

    trial

--- src/Travel.js
+++ src/Travel.js
@@ -10,4 +10,44 @@ const Travel = () => {
   return <div>Travel Component</div>;
 };
 
+import React, { useState } from "react";
+
+const DoubleComponent = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleDoubleClick = () => {
+    const inputValue = parseFloat(input);
+    if (!isNaN(inputValue)) {
+      setResult(inputValue * 2);
+    } else {
+      setResult(null);
+    }
+  };
+
+  return (
+    <div>
+      <label>
+        Enter a number:
+        <input
+          type='text'
+          value={input}
+          onChange={handleInputChange}
+          placeholder='Enter a number'
+        />
+      </label>
+      <button onClick={handleDoubleClick}>Double</button>
+      {result !== null && (
+        <p>
+          The double of {input} is: {result}
+        </p>
+      )}
+    </div>
+  );
+};
+
 export default Travel;

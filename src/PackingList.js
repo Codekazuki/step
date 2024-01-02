7fd47e40b7c8757eea01f69c170ccf20e95309be
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-02 14:04:18 +0100

    travel list app list component

--- src/PackingList.js
+++ src/PackingList.js
@@ -1,24 +1,34 @@
 import React from "react";
 
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Brush", quantity: 1, packed: false },
 ];
 
 const PackingList = () => {
   return (
-    <div>
-      {initialItems.map((item) => {
-        return (
-          <ul>
-            <h3 key={item.quantity}>{item.quantity}</h3>
-            <h2 key={item.id}>{item.description}</h2>
-          </ul>
-        );
-      })}
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
     </div>
   );
 };
-
+function Item({ item }) {
+  return (
+    <li>
+      <span
+        style={
+          item.packed ? { textDecoration: "line-through", color: "green" } : {}
+        }
+      >
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
 export default PackingList;

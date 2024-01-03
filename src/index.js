1f3f47c82fefc9ee90e244fefc1d79faa99a13ed
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-03 10:43:31 +0100

    flashcards starter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 // import App from "./App";
 // import Date from "./Date";
 import Travel from "./Travel";
+import Flashcards from "./Flashcards";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,5 +12,6 @@ root.render(
     {/* <App /> */}
     {/* <Date /> */}
     <Travel />
+    <Flashcards />
   </React.StrictMode>
 );

12dd30b03e6e0789556b13b71fcd5098fe2928fb
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-03 16:00:33 +0100

    date counter

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import Date from "./Date";
 import Travel from "./Travel";
 import Flashcards from "./Flashcards";
+import DateCounter from "./DateCounter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Date />
+    <DateCounter />
     <Travel />
     <Flashcards />
   </React.StrictMode>

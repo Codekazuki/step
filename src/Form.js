08cde8a1f2c89cb5f0750870e0ccbc0cb61574c7
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-03 09:28:18 +0100

    travel list app form

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 const Form = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submitted");
+  }
   return (
-    <div className='add-form'>
-      <h2>Forms goes here</h2>
-    </div>
+    <form onSubmit={handleSubmit} className='add-form'>
+      <h2>What do you want to pack?</h2>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type='text' placeholder='item' />
+      <button>Add Item</button>
+    </form>
   );
 };
 

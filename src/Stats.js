f7dd48b4a9c08f8db634b29230c858c910884bb9
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-08 17:25:48 +0100

    percentage

--- src/Stats.js
+++ src/Stats.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const Stats = ({ items }) => {
   const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.floor((packedItems / numItems) * 100);
   return (
     <footer className='stats'>
-      <em>You have {numItems} items </em>
+      <em>
+        You have {numItems} items ,and {packedItems} have been parked{" "}
+        {percentage}%
+      </em>
     </footer>
   );
 };

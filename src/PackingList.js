0a0ea398706d56988216fb0b7dc404bb9f76abb8
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-09 09:46:05 +0100

    sorting

--- src/PackingList.js
+++ src/PackingList.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  if (sortBy === "quantity")
+    sortedItems = items.slice().sort((a, b) => b.quantity - a.quantity);
   return (
     <div className='list'>
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             onToggleItem={onToggleItem}
@@ -14,10 +27,11 @@ const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
         ))}
       </ul>
       <div className='actions'>
-        <select>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <option value='input'>sort by input order</option>
           <option value='description'>Sort by description</option>
           <option value='packed'>Sort by packed status</option>
+          <option value='quantity'>Sort by quantity</option>
         </select>
       </div>
     </div>
